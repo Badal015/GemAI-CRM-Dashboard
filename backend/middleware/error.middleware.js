9841468362387d37acc931b293f61214204ad5f2
Badal Hotha <badalhotha131@gmail.com>
2026-07-09 20:50:55 +0530

    Fix ApiError import case

--- backend/middleware/error.middleware.js
+++ backend/middleware/error.middleware.js
@@ -1,4 +1,4 @@
-import { ApiError } from '../utils/apiError.js';
+import { ApiError } from '../utils/ApiError.js';
 export const notFound = (req, res, next) => {
     next(new ApiError(404, ` Route Not Found - ${req.originalUrl}`));
 };
